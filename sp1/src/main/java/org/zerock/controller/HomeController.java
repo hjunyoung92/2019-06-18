e674fd04e88a152e529e4b9280120ffc39830b90
bitcamp13 <bitcamp13@bitcamp>
2019-06-18 15:30:01 +0900

    테스트 환경 준비

--- sp1/src/main/java/org/zerock/controller/HomeController.java
+++ sp1/src/main/java/org/zerock/controller/HomeController.java
@@ -6,10 +6,12 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.service.HelloService;
 
 /**
  * Handles requests for the application home page.
@@ -18,12 +20,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+	@Autowired /*너 이탑의 빈있어? 있으면 넣어줘*/
+	private HelloService service;
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+		
+		logger.info("-----------------");
+		logger.info(""+service);
+		logger.info("-----------------");
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
