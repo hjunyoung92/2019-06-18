1baa97b6a9842f653472aaa6a8437938b0a59bdf
bitcamp13 <bitcamp13@bitcamp>
2019-06-19 14:29:39 +0900

    6/19

--- sp1/src/test/java/org/zerock/service/HelloServiceTests.java
+++ sp1/src/test/java/org/zerock/service/HelloServiceTests.java
@@ -1,5 +1,37 @@
 package org.zerock.service;
 
-public class HelloServiceTests {
+import static org.junit.Assert.assertNotNull;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class HelloServiceTests {
+	
+	@Setter(onMethod_ = {@Autowired} )
+	private HelloService Service;
+	
+	@Autowired
+	private Restaurant restaurant;
+	
+	@Test
+	public void testChef() {
+		restaurant.order();
+	}
+	
+	@Test
+	public void testExist() {
+		log.info(Service);
+		assertNotNull(Service);
+	}
+	
+	
 }
